dbabd1ba6d3b3143b30fde340f97e3f2367fb0b3
Erik Williamson <me@erik.tw>
2019-06-26 18:17:37 -0400

    refactor Statistics out to use context

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,26 +1,39 @@
 import * as React from 'react'
-import { Row, Col, Statistic } from 'antd';
-import {State} from './AppStateWrapper'
-/*
-const justDollars = range.map(dollar => dollar.value)
-const average = justDollars.reduce(sum, 0) / justDollars.length
-const minimum = justDollars.reduce(min, Infinity)
-const maximum = justDollars.reduce(max, -Infinity)
+import {
+    Row,
+    Col,
+    Statistic,
+} from 'antd';
+import { AppContext } from './AppStateWrapper'
 
 export const Statistics = () => {
     return (
+        <AppContext.Consumer>
+            {value => {
+                const { range } = value
+                const justDollars = range.map(dollar => dollar.value)
+                const sum = (accumulator: number, currentValue: number) => accumulator + currentValue
+                const max = (a: number, b: number) => (Math.max(a, b))
+                const min = (a: number, b: number) => (Math.min(a, b))
+                const average = justDollars.reduce(sum, 0) / justDollars.length
+                const minimum = justDollars.reduce(min, Infinity)
+                const maximum = justDollars.reduce(max, -Infinity)
 
-        <Row gutter={16}>
-            <Col span={8}>
-                <Statistic title="⬆ High" value={maximum} />
-            </Col>
-            <Col span={8}>
-                <Statistic title="⬇ Low" value={minimum} />
-            </Col>
-            <Col span={8}>
-                <Statistic title="⇔ Average" value={average} precision={2} />
-            </Col>
-        </Row>
+                return (
+                    <Row gutter={16}>
+                        <Col span={8}>
+                            <Statistic title="⬆ High" value={maximum} />
+                        </Col>
+                        <Col span={8}>
+                            <Statistic title="⬇ Low" value={minimum} />
+                        </Col>
+                        <Col span={8}>
+                            <Statistic title="⇔ Average" value={average} precision={2} />
+                        </Col>
+                    </Row>
+                )
+            }
+            }
+        </AppContext.Consumer>
     )
 }
-*/
