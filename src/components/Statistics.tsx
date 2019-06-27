22913df3d3ef14ad2cd805203a328aa156f93300
Erik Williamson <me@erik.tw>
2019-06-26 20:24:57 -0400

    add localized text

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -6,11 +6,25 @@ import {
 } from 'antd';
 import { AppContext } from './AppStateWrapper'
 
+interface LocalizedAppValue {
+    [key: string]: {
+        en_US: string
+        es_ES: string
+    }
+}
+
+const text: LocalizedAppValue = {
+    statisticTitleHigh: { en_US: 'High', es_ES: 'Maximo' },
+    statisticTitleLow: { en_US: 'Low', es_ES: 'Minimo' },
+    statisticTitleAverage: { en_US: 'Average', es_ES: 'Promedio' },
+}
+
 export const Statistics = () => {
     return (
         <AppContext.Consumer>
             {value => {
                 const { range } = value
+                const locale = value.locale.locale
                 const justDollars = range.map(dollar => dollar.value)
                 const sum = (accumulator: number, currentValue: number) => accumulator + currentValue
                 const max = (a: number, b: number) => (Math.max(a, b))
@@ -22,18 +36,17 @@ export const Statistics = () => {
                 return (
                     <Row gutter={16}>
                         <Col span={8}>
-                            <Statistic title="⬆ High" value={maximum} />
+                            <Statistic title={`⬆ ${locale === 'es' ? text.statisticTitleHigh.es_ES : text.statisticTitleHigh.en_US}`} value={maximum} />
                         </Col>
                         <Col span={8}>
-                            <Statistic title="⬇ Low" value={minimum} />
+                            <Statistic title={`⬇ ${locale === 'es' ? text.statisticTitleLow.es_ES : text.statisticTitleLow.en_US}`} value={minimum} />
                         </Col>
                         <Col span={8}>
-                            <Statistic title="⇔ Average" value={average} precision={2} />
+                            <Statistic title={`⇔ ${locale === 'es' ? text.statisticTitleAverage.es_ES : text.statisticTitleAverage.en_US}`} value={average} precision={2} />
                         </Col>
                     </Row>
                 )
-            }
-            }
+            }}
         </AppContext.Consumer>
     )
 }
